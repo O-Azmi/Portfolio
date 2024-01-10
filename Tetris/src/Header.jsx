6439624d8c99b26fd401482c92dc5899dbe87c0e
Oazmi <azmioualid@gmail.com>
2024-01-09 20:01:06 -0600

    Title colors/Setting up
    Changed minimal stuff, now onto ERD design

---
+++ Tetris/src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function Header(){
+    return(
+    <header>
+        <nav>
+            <ul>
+                <li className="page-title">Tetris Project</li>
+            </ul>
+            </nav>
+    </header>
+    )
+}
